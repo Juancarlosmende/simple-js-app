508e2d3c6527026a26f06963edd27fb70c8993c4
Juancarlosmende <128351394+Juancarlosmende@users.noreply.github.com>
2023-06-02 10:53:30 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -60,7 +60,7 @@ let pokemonRepository = (function () {
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
     showDetails: showDetails,
   };
  
